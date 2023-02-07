6c8e66facc9f135dcd4d09737655e397e16ddbbc
Mateus Belicio <mateusbelicio@hotmail.com>
2023-02-07 17:48:39 -0300

    Finished project

--- js/input.js
+++ js/input.js
@@ -1,97 +1,311 @@
 class Input {
   valid = false;
-  _typesDefinitions = {
-    number: {
-      placeholder: '0000 0000 0000 0000',
-      mask(value) {
-        return value
-          .replace(/\D/g, '')
-          .replace(/(\d{4})(\d)/, '$1 $2')
-          .replace(/(\d{4})(\d)/, '$1 $2')
-          .replace(/(\d{4})(\d)/, '$1 $2')
-          .replace(/(\s\d{4})\d+?$/, '$1');
-      },
-    },
-    name: {
-      placeholder: 'Jane Appleseed',
-      mask(value) {
-        return value.replace(/[0-9!@#¨$%^&*)(+=._,.;/[{}?'"\]\\-]+/g, '');
-      },
-    },
-    month: {
-      placeholder: '00',
-      mask(value) {
-        return value
-          .replace(/\D/g, '')
-          .replace(/^([2-9]{1})/, '0$1')
-          .replace(/^(0)0/, '$1')
-          .replace(/^(0[1-9])\d/, '$1')
-          .replace(/^(1[0-2])\d/, '$1')
-          .replace(/^(1)([3-9])/, '$1');
-      },
-    },
-    year: {
-      placeholder: '00',
-      mask(value) {
-        return value.replace(/\D/g, '').replace(/(\d{2})\d+?$/, '$1');
-      },
-    },
-    cvc: {
-      placeholder: '000',
-      mask(value) {
-        return value.replace(/\D/g, '').replace(/(\d{3})\d+?$/, '$1');
-      },
-    },
-  };
 
   constructor(input) {
-    this.input = input;
+    this._input = input;
+    this._type = input.id;
     this._field = input.closest('.form__field');
     this._error = this._field.querySelector('.form__error-msg');
-    this._type = input.id;
     this._cardElement = document.querySelector(`.card__${this._type}`);
 
     this._addHandlerInputValue();
   }
 
-  renderError(msg = "Can't be blank") {
-    this._error.textContent = msg;
-    this.input.dataset.valid = false;
+  /**
+   * Checks whether the validation is correct or not
+   * @public
+   * @returns {Boolean} [true | false]
+   */
+  isValidValue() {
+    return this._validade(this._input.value);
+  }
+
+  /**
+   * Checks if the current input is empty
+   * @public
+   * @returns {Boolean} [true | false] - true if input is empty and false otherwise
+   */
+  isBlank() {
+    return this._input.value === '' ? true : false;
+  }
+
+  /**
+   * Renders the error message according to the error type
+   * @public
+   * @param {Boolean} blankMsg - If true, the error message will indicate that the input is empty, otherwise it will indicate that there is an error in the format of the input
+   */
+  renderErrorMessage(blankMsg = true) {
+    this._error.textContent = blankMsg ? "Can't be blank" : this.errorMessage;
+  }
+
+  /**
+   * Renders the error alert
+   * @public
+   */
+  renderErrorAlert() {
+    this._input.dataset.valid = false;
     this.valid = false;
   }
 
-  clearError() {
+  /**
+   * Clears the messages that indicate the error
+   * @public
+   */
+  clearErrorMessage() {
     this._error.textContent = '';
-    this.input.dataset.valid = true;
+  }
+
+  /**
+   * Clears the alerts that indicate the error
+   * @public
+   */
+  clearErrorAlert() {
+    this._input.dataset.valid = true;
     this.valid = true;
   }
 
+  /**
+   * Clears the error messages, the current input values ​​and the data displayed on the card
+   * @public
+   */
   clearAll() {
-    this.clearError();
-    this.input.value = '';
+    this.clearErrorMessage();
+    this.clearErrorAlert();
+    this._input.value = '';
+    this.renderValue();
   }
 
-  _renderValue() {
-    this._cardElement.textContent =
-      this.input.value || this._typesDefinitions[this._type].placeholder;
+  /**
+   * Render the input value on the card
+   * @public
+   */
+  renderValue() {
+    this._cardElement.textContent = this._input.value || this._placeholder;
   }
 
+  /**
+   * Changes the input value to match the mask value
+   * @private
+   */
   _maskValue() {
-    this.input.value = this._typesDefinitions[this._type].mask(this.input.value);
+    this._input.value = this._mask(this._input.value);
   }
 
+  /**
+   * Adds an event handler on inputs
+   * @private
+   */
   _addHandlerInputValue() {
-    this.input.addEventListener('input', () => {
+    this._input.addEventListener('input', () => {
       this._maskValue();
-      this._renderValue();
+      this.renderValue();
     });
   }
 }
 
-const inputName = new Input(document.getElementById('name'));
-const inputNumber = new Input(document.getElementById('number'));
-const inputMonth = new Input(document.getElementById('month'));
-const inputYear = new Input(document.getElementById('year'));
-const inputCode = new Input(document.getElementById('cvc'));
+// CLASS NUMBER =================================================
+class InputNumber extends Input {
+  _placeholder = '0000 0000 0000 0000';
+  errorMessage = 'Wrong format, some numbers are missing';
 
-export const inputs = [inputName, inputNumber, inputMonth, inputYear, inputCode];
+  constructor(input) {
+    super(input);
+  }
+
+  /**
+   * Changes the input value to match the allowed and desired input type
+   * @private
+   * @param {String} value - the current value on the input
+   * @returns {String} the changed value to match the mask value
+   */
+  _mask(value) {
+    return value
+      .replace(/\D/g, '')
+      .replace(/(\d{4})(\d)/, '$1 $2')
+      .replace(/(\d{4})(\d)/, '$1 $2')
+      .replace(/(\d{4})(\d)/, '$1 $2')
+      .replace(/(\s\d{4})\d+?$/, '$1');
+  }
+
+  /**
+   * Checks if the input value is valid
+   * @private
+   * @param {String} value - the current value on the input
+   * @returns {Boolean} [true | false] - if true, the input is valid and no errors are displayed
+   */
+  _validade(value) {
+    return value.length < this._placeholder.length ? false : true;
+  }
+}
+
+// CLASS NAME =================================================
+class InputName extends Input {
+  _placeholder = 'Jane Appleseed';
+  errorMessage = 'Wrong format';
+
+  constructor(input) {
+    super(input);
+  }
+
+  /**
+   * Changes the input value to match the allowed and desired input type
+   * @private
+   * @param {String} value - the current value on the input
+   * @returns {String} the changed value to match the mask value
+   */
+  _mask(value) {
+    return value.replace(/[0-9!@#¨$%^&*)(+=._,.;/[{}?'"\]\\-]+/g, '');
+  }
+
+  /**
+   * Checks if the input value is valid
+   * @private
+   * @param {String} value - the current value on the input
+   * @returns {Boolean} [true | false] - if true, the input is valid and no errors are displayed
+   */
+  _validade(value) {
+    const invalidsChar = /([()[\]{},.;~^´`-])/;
+
+    if ([...value].some((char) => invalidsChar.test(char))) return false;
+    if (![...value].some((char) => char === ' ')) return false;
+    if (value.split(' ').some((word) => word.length === 0)) return false;
+
+    return true;
+  }
+}
+
+// CLASS MONTH =================================================
+class InputMonth extends Input {
+  _placeholder = '00';
+  errorMessage = 'Invalid date';
+
+  constructor(input) {
+    super(input);
+  }
+
+  /**
+   * Checks if the month entered is valid
+   * @returns {Boolean} [true | false] - True is the month is valid
+   */
+  dateIsValid() {
+    const month = +this._field.querySelector('#month').value;
+    const year = +this._field.querySelector('#year').value;
+
+    const date = new Date().toISOString().split('-');
+    const currentMonth = +date[1];
+    const currentYear = +date[0].slice(-2);
+
+    if (year === currentYear && month < currentMonth) return false;
+    return true;
+  }
+
+  /**
+   * Changes the input value to match the allowed and desired input type
+   * @private
+   * @param {String} value - the current value on the input
+   * @returns {String} the changed value to match the mask value
+   */
+  _mask(value) {
+    return value
+      .replace(/\D/g, '')
+      .replace(/^([1-9]{1})/, '0$1')
+      .replace(/^(0)0/, '$1')
+      .replace(/^(0[2-9])\d/, '$1')
+      .replace(/^0(1[0-2])/, '$1')
+      .replace(/^(01)[3-9]/, '$1')
+      .replace(/^(1[0-2])\d/, '$1')
+      .replace(/^(1)([3-9])/, '$1');
+  }
+
+  /**
+   * Checks if the input value is valid
+   * @private
+   * @param {String} value - the current value on the input
+   * @returns {Boolean} [true | false] - if true, the input is valid and no errors are displayed
+   */
+  _validade(value) {
+    return +value >= 1 && +value <= 12 && this.dateIsValid() ? true : false;
+  }
+}
+
+// CLASS YEAR =================================================
+class InputYear extends Input {
+  _placeholder = '00';
+  errorMessage = 'Invalid date';
+
+  constructor(input) {
+    super(input);
+  }
+
+  /**
+   * Checks if the year entered is valid
+   * @returns {Boolean} [true | false] - True is the year is valid
+   */
+  dateIsValid() {
+    const month = +this._field.querySelector('#month').value;
+    const year = +this._field.querySelector('#year').value;
+
+    const date = new Date().toISOString().split('-');
+    const currentYear = +date[0].slice(-2);
+
+    if (year < currentYear || year > currentYear + 10) return false;
+    return true;
+  }
+
+  /**
+   * Changes the input value to match the allowed and desired input type
+   * @private
+   * @param {String} value - the current value on the input
+   * @returns {String} the changed value to match the mask value
+   */
+  _mask(value) {
+    return value.replace(/\D/g, '').replace(/(\d{2})\d+?$/, '$1');
+  }
+
+  /**
+   * Checks if the input value is valid
+   * @private
+   * @param {String} value - the current value on the input
+   * @returns {Boolean} [true | false] - if true, the input is valid and no errors are displayed
+   */
+  _validade(value) {
+    if (value.length < 2 || !this.dateIsValid()) return false;
+    return true;
+  }
+}
+
+// CLASS CODE =================================================
+class InputCode extends Input {
+  _placeholder = '000';
+  errorMessage = 'Wrong format, some numbers are missing';
+
+  constructor(input) {
+    super(input);
+  }
+
+  /**
+   * Changes the input value to match the allowed and desired input type
+   * @private
+   * @param {String} value - the current value on the input
+   * @returns {String} the changed value to match the mask value
+   */
+  _mask(value) {
+    return value.replace(/\D/g, '').replace(/(\d{3})\d+?$/, '$1');
+  }
+
+  /**
+   * Checks if the input value is valid
+   * @private
+   * @param {String} value - the current value on the input
+   * @returns {Boolean} [true | false] - if true, the input is valid and no errors are displayed
+   */
+  _validade(value) {
+    return value.length < this._placeholder.length ? false : true;
+  }
+}
+
+export const name = new InputName(document.getElementById('name'));
+export const number = new InputNumber(document.getElementById('number'));
+export const month = new InputMonth(document.getElementById('month'));
+export const year = new InputYear(document.getElementById('year'));
+export const code = new InputCode(document.getElementById('cvc'));
--- js/main.js
+++ js/main.js
@@ -1,26 +1,66 @@
-import { inputs } from './input.js';
+import * as inputs from './input.js';
 
+// Selecting DOM elements
 const form = document.querySelector('.form');
 const continueButton = document.querySelector('.details__complete-state .btn');
 
-const validadeInputs = function () {
-  inputs.forEach((i) => {
-    if (i.input.value.length === 0) i.renderError();
-    else i.clearError();
+// Separating inputs according to their fieldsets
+const dateInputs = [inputs.month, inputs.year];
+const noDateInputs = [inputs.name, inputs.number, inputs.code];
+const allInputs = [inputs.name, inputs.number, inputs.month, inputs.year, inputs.code];
+
+/**
+ * Controls the form checking all inputs to validade and submit the form
+ * @param {Event} event - stores the event object with information about the triggered event
+ */
+const controlForm = function (event) {
+  event.preventDefault();
+
+  // Checking if the inputs that not represents the expiration date are valids
+  noDateInputs.forEach((i) => {
+    if (i.isValidValue()) {
+      i.clearErrorAlert();
+      i.clearErrorMessage();
+    } else if (i.isBlank()) {
+      i.renderErrorAlert();
+      i.renderErrorMessage();
+    } else {
+      i.renderErrorMessage(false);
+      i.renderErrorAlert();
+    }
   });
 
-  if (inputs.every((i) => i.valid === true)) {
+  // Checking if the expiration date inputs are valids
+  dateInputs.forEach((i) => {
+    if (i.isValidValue()) {
+      i.clearErrorAlert();
+    } else if (i.isBlank()) {
+      i.renderErrorAlert();
+      i.renderErrorMessage();
+    } else {
+      i.renderErrorMessage(false);
+      i.renderErrorAlert();
+    }
+  });
+
+  // Checking if ALL the expiration date inputs are valids to clear the error message
+  dateInputs.every((i) => i.isValidValue()) && inputs.year.clearErrorMessage();
+
+  // Submiting the form case all inputs are valids
+  if (allInputs.every((i) => i.valid === true)) {
     document.querySelector('.details__form').dataset.validaded = true;
   }
 };
 
-const controlForm = function (event) {
-  event.preventDefault();
-  validadeInputs();
-};
-
+//////////////////////////////////////////////////////
+// EVENTS HANDLERS
 form.addEventListener('submit', controlForm);
+
 continueButton.addEventListener('click', () => {
   document.querySelector('.details__form').dataset.validaded = false;
-  inputs.forEach((i) => i.clearAll());
+  allInputs.forEach((i) => i.clearAll());
+});
+
+window.addEventListener('load', () => {
+  allInputs.forEach((i) => i.renderValue());
 });
